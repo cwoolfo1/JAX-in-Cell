6277955969a14210d10e59413c0641391f547236
cwoolfo1 <70346478+cwoolfo1@users.noreply.github.com>
2025-07-09 16:39:13 -0500

    more changes

--- jaxincell/_relativity.py
+++ jaxincell/_relativity.py
@@ -85,6 +85,7 @@ def trace_energy_momentum_tensor(a1, vs, ms):
 
     C = speed_of_light
     # # Calculate the four-velocity for each particle
+        # gamma = jnp.sqrt(  (a1_at_x * vx_nplushalf / C )**2  - a1_at_x )
     four_velocity = jax.vmap(lambda v : jax.numpy.array([ C * jax.numpy.sqrt(1 + (v[0]**2 / C**2)),  v[0] ]), in_axes=(0))(vs)
     # v = (v0, gamma * C) defining four vector velocity
     trace = jax.vmap(lambda v, m: m * (   a1 * v[1]**2 - (v[0]**2 / a1)   ), in_axes=(0,0))(four_velocity, ms)
@@ -280,6 +281,7 @@ def relativistic_electrostatic_step(xs_nplushalf, vs_n, q_ms, E_fields_at_x, a2,
     # Get the x-component of the particle velocities at time step n+1/2
 
     gamma = jnp.sqrt(1 + (vx_nplushalf/C)**2 )
+    # gamma = jnp.sqrt(  (a1_at_x * vx_nplushalf / C )**2  - a1_at_x )
     # Calculate the relativistic factor gamma at the particle positions
 
     vx_nplus1 = vx_nplushalf -1 * (q_ms[:,0]) * E_fields_at_x[:, 0] * a1_at_x * dt + \
